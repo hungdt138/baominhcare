9b6ff516a6e70cf3fc2cf7d187cd6b66d70fb1e4
Do Tien Hung <hungdt.it@gmail.com>
2019-04-02 18:21:01 +0700

    no message

--- src/main/java/com/baominh/sdp/controller/VNMSDPController.java
+++ src/main/java/com/baominh/sdp/controller/VNMSDPController.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.baominh.sdp.controller;
 
@@ -35,51 +35,51 @@ import com.baominh.sdp.utils.LoggingUtils;
 @RestController
 @RequestMapping("/api/vnm")
 public class VNMSDPController {
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@Autowired
-	private SubscriberService subscriberService;
+    @Autowired
+    private SubscriberService subscriberService;
 
-	@Autowired
-	private VNMSDPService vnmSdpService;
+    @Autowired
+    private VNMSDPService vnmSdpService;
 
-	@Autowired
-	private ServiceRepository serviceRepository;
+    @Autowired
+    private ServiceRepository serviceRepository;
 
-	@Autowired
-	private ContentRepository contentRepository;
+    @Autowired
+    private ContentRepository contentRepository;
 
-	@Autowired
-	private RabbitMQPublisherImpl rabbitMqPublisherImpl;
+    @Autowired
+    private RabbitMQPublisherImpl rabbitMqPublisherImpl;
 
-	@Autowired
-	private SettingRepository settingRepository;
+    @Autowired
+    private SettingRepository settingRepository;
 
-	@RequestMapping(value = "/status", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_UTF8_VALUE })
-	public ResponseDto checkStatus() {
-		logger.info("Check status done ...");
-		ResponseDto resp = new ResponseDto();
-		resp.setDescription("Success");
-		resp.setStatus("0");
-		return resp;
-	}
+    @RequestMapping(value = "/status", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseDto checkStatus() {
+        logger.info("Check status done ...");
+        ResponseDto resp = new ResponseDto();
+        resp.setDescription("Success");
+        resp.setStatus("0");
+        return resp;
+    }
 
-	@RequestMapping(value = "/subscriber", method = RequestMethod.GET, produces = {
-			MediaType.APPLICATION_JSON_UTF8_VALUE })
-	public ResponseDto updateSubscriber(@PathParam("id") String id, @PathParam("transid") String transid,
-			@PathParam("isdn") String isdn, @PathParam("serviceaddress") String serviceaddress,
-			@PathParam("categoryid") String categoryid, @PathParam("productid") String productid,
-			@PathParam("command") String command, @PathParam("amount") String amount,
-			@PathParam("expiration") String expiration, @PathParam("hash") String hash) {
+    @RequestMapping(value = "/subscriber", method = RequestMethod.GET, produces = {
+            MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseDto updateSubscriber(@PathParam("id") String id, @PathParam("transid") String transid,
+                                        @PathParam("isdn") String isdn, @PathParam("serviceaddress") String serviceaddress,
+                                        @PathParam("categoryid") String categoryid, @PathParam("productid") String productid,
+                                        @PathParam("command") String command, @PathParam("amount") String amount,
+                                        @PathParam("expiration") String expiration, @PathParam("hash") String hash) {
 
-		SubscriberRequestDto subRequest = SubscriberRequestDto.builder().id(id).transid(transid).isdn(isdn)
-				.serviceaddress(serviceaddress).categoryid(categoryid).productid(productid).command(command)
-				.amount(amount).expiration(expiration).hash(hash).build();
+        SubscriberRequestDto subRequest = SubscriberRequestDto.builder().id(id).transid(transid).isdn(isdn)
+                .serviceaddress(serviceaddress).categoryid(categoryid).productid(productid).command(command)
+                .amount(amount).expiration(expiration).hash(hash).build();
 
-		logger.info("SDP Request: {}", LoggingUtils.objToStringIgnoreEx(subRequest));
+        logger.info("SDP Request: {}", LoggingUtils.objToStringIgnoreEx(subRequest));
 
-		if (subRequest.getCommand().equalsIgnoreCase(Constants.SDP_COMMAND.REGISTER)) {
-			subscriberService.register(subRequest.getIsdn(), Integer.valueOf(subRequest.getProductid()));
+        if (subRequest.getCommand().equalsIgnoreCase(Constants.SDP_COMMAND.REGISTER)) {
+            subscriberService.register(subRequest.getIsdn(), Integer.valueOf(subRequest.getProductid()));
 //			ServiceEntity service = serviceRepository
 //					.getServiceBySdpProductId(Integer.valueOf(subRequest.getProductid()));
 //
@@ -90,54 +90,54 @@ public class VNMSDPController {
 //					.build();
 //
 //			rabbitMqPublisherImpl.putToSMSQueue(sendSms);
-		} else if (subRequest.getCommand().equalsIgnoreCase(Constants.SDP_COMMAND.SUBSCRIPTION)) {
-			subscriberService.subscription(subRequest);
-		} else { // unregister or cancel
-			subscriberService.unregister(subRequest.getIsdn(), Integer.valueOf(subRequest.getProductid()));
-		}
+        } else if (subRequest.getCommand().equalsIgnoreCase(Constants.SDP_COMMAND.SUBSCRIPTION)) {
+            subscriberService.subscription(subRequest);
+        } else { // unregister or cancel
+            subscriberService.unregister(subRequest.getIsdn(), Integer.valueOf(subRequest.getProductid()));
+        }
 
-		ResponseDto resp = new ResponseDto();
-		resp.setDescription("Success");
-		resp.setStatus("0");
-		return resp;
-	}
+        ResponseDto resp = new ResponseDto();
+        resp.setDescription("Success");
+        resp.setStatus("0");
+        return resp;
+    }
 
-	@RequestMapping(value = "/moevent", method = RequestMethod.GET, produces = {
-			MediaType.APPLICATION_JSON_UTF8_VALUE })
-	public ResponseDto moEvent(@PathParam("id") String id, @PathParam("transid") String transid,
-			@PathParam("isdn") String isdn, @PathParam("serviceaddress") String serviceaddress,
-			@PathParam("categoryid") String categoryid, @PathParam("productid") String productid,
-			@PathParam("moid") String moid, @PathParam("amount") String amount, @PathParam("message") String message,
-			@PathParam("hash") String hash) {
+    @RequestMapping(value = "/moevent", method = RequestMethod.GET, produces = {
+            MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseDto moEvent(@PathParam("id") String id, @PathParam("transid") String transid,
+                               @PathParam("isdn") String isdn, @PathParam("serviceaddress") String serviceaddress,
+                               @PathParam("categoryid") String categoryid, @PathParam("productid") String productid,
+                               @PathParam("moid") String moid, @PathParam("amount") String amount, @PathParam("message") String message,
+                               @PathParam("hash") String hash) {
 
-		MORequestDto moRequest = MORequestDto.builder().id(id).transid(transid).isdn(isdn)
-				.serviceaddress(serviceaddress).categoryid(categoryid).productid(productid).moid(moid).amount(amount)
-				.message(message).hash(hash).build();
+        MORequestDto moRequest = MORequestDto.builder().id(id).transid(transid).isdn(isdn)
+                .serviceaddress(serviceaddress).categoryid(categoryid).productid(productid).moid(moid).amount(amount)
+                .message(message).hash(hash).build();
 
-		logger.info("SDP Request: {}", LoggingUtils.objToStringIgnoreEx(moRequest));
+        logger.info("SDP Request: {}", LoggingUtils.objToStringIgnoreEx(moRequest));
 
-		if (Constants.SDP_MO.KTDV.equalsIgnoreCase(message)) {
-			List<String> lstSubContentNoti = subscriberService.checkSubscription(isdn);
+        if (Constants.SDP_MO.KTDV.equalsIgnoreCase(message)) {
+            List<String> lstSubContentNoti = subscriberService.checkSubscription(isdn);
 
-			SendSmsDto sendSms = null;
+            SendSmsDto sendSms = null;
 
-			for (int i = 0; i < lstSubContentNoti.size(); i++) {
-				sendSms = SendSmsDto.builder().content(lstSubContentNoti.get(i)).flash(0).href("").isdn(isdn).moId(moid)
-						.serviceAddress("568").serviceId(0).unicode(0).build();
-				rabbitMqPublisherImpl.putToSMSQueue(sendSms);
+            for (int i = 0; i < lstSubContentNoti.size(); i++) {
+                sendSms = SendSmsDto.builder().content(lstSubContentNoti.get(i)).flash(0).href("").isdn(isdn).moId(moid)
+                        .serviceAddress("568").serviceId(0).unicode(0).build();
+                rabbitMqPublisherImpl.putToSMSQueue(sendSms);
 
-				logger.info(LoggingUtils.objToStringIgnoreEx(sendSms));
+                logger.info(LoggingUtils.objToStringIgnoreEx(sendSms));
 
-			}
-		} else if (Constants.SDP_MO.HDSD.equalsIgnoreCase(message)) {
-			
-		} else if (Constants.SDP_MO.GIA.equalsIgnoreCase(message)) {
-			
-		}
+            }
+        } else if (Constants.SDP_MO.HDSD.equalsIgnoreCase(message)) {
 
-		ResponseDto resp = new ResponseDto();
-		resp.setDescription("Success");
-		resp.setStatus("0");
-		return resp;
-	}
+        } else if (Constants.SDP_MO.GIA.equalsIgnoreCase(message)) {
+
+        }
+
+        ResponseDto resp = new ResponseDto();
+        resp.setDescription("Success");
+        resp.setStatus("0");
+        return resp;
+    }
 }
--- src/main/java/com/baominh/sdp/service/impl/SubscriberServiceImpl.java
+++ src/main/java/com/baominh/sdp/service/impl/SubscriberServiceImpl.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.baominh.sdp.service.impl;
 
@@ -38,127 +38,127 @@ import java.util.List;
 @Slf4j
 public class SubscriberServiceImpl implements SubscriberService {
 
-	@Autowired
-	private ServiceRepository serviceRepository;
+    @Autowired
+    private ServiceRepository serviceRepository;
 
-	@Autowired
-	private SmsuserRepository smsUserRepository;
+    @Autowired
+    private SmsuserRepository smsUserRepository;
 
-	@Autowired
-	private VNMSDPService vnmSdpService;
+    @Autowired
+    private VNMSDPService vnmSdpService;
 
-	@Autowired
-	private ChargingLogRepository chargingLogRepository;
-	
-	@Autowired
-	private ChargingRepository chargingRepository;
+    @Autowired
+    private ChargingLogRepository chargingLogRepository;
 
-	@Autowired
-	private SettingRepository settingRepository;
+    @Autowired
+    private ChargingRepository chargingRepository;
 
-	@Override
-	@Transactional
-	public void register(String isdn, Integer sdpProductId) {
-		log.info("Register subscriber. isdn = {}, productId = {}", isdn, sdpProductId);
-		ServiceEntity service = serviceRepository.getServiceBySdpProductId(sdpProductId);
-		if (service != null) {
-			Smsuser subscriber = smsUserRepository.getSubscriberByIsdn(service.getId(), "+" + isdn);
-			if (subscriber != null && subscriber.getActive() == 1) {
-				subscriber.setUnregisterDate(DateHelper.dateToUnixTime(new Date()));
-				subscriber.setActive(0);
-				smsUserRepository.save(subscriber);
-			}
-			Smsuser user = Smsuser.builder().active(1).phone("+" + isdn)
-					.registerDate(DateHelper.dateToUnixTime(new Date())).registerType("SDP").serviceID(service.getId())
-					.build();
-			smsUserRepository.save(user);
-			log.info("Save user {} successfuly. serciceId = {}", isdn, service.getId());
+    @Autowired
+    private SettingRepository settingRepository;
 
-			// vnmSdpService.sendSMS(isdn, service.getId());
+    @Override
+    @Transactional
+    public void register(String isdn, Integer sdpProductId) {
+        log.info("Register subscriber. isdn = {}, productId = {}", isdn, sdpProductId);
+        ServiceEntity service = serviceRepository.getServiceBySdpProductId(sdpProductId);
+        if (service != null) {
+            Smsuser subscriber = smsUserRepository.getSubscriberByIsdn(service.getId(), "+" + isdn);
+            if (subscriber != null && subscriber.getActive() == 1) {
+                subscriber.setUnregisterDate(DateHelper.dateToUnixTime(new Date()));
+                subscriber.setActive(0);
+                smsUserRepository.save(subscriber);
+            }
+            Smsuser user = Smsuser.builder().active(1).phone("+" + isdn)
+                    .registerDate(DateHelper.dateToUnixTime(new Date())).registerType("SDP").serviceID(service.getId())
+                    .build();
+            smsUserRepository.save(user);
+            log.info("Save user {} successfuly. serciceId = {}", isdn, service.getId());
 
-		} else {
-			log.info("Service not found with SDPPRODUCTID = ", sdpProductId);
-		}
-	}
+            // vnmSdpService.sendSMS(isdn, service.getId());
 
-	@Override
-	public void unregister(String isdn, Integer sdpProductId) {
-		log.info("unregister subscriber. isdn = {}, productId = {}", isdn, sdpProductId);
-		ServiceEntity service = serviceRepository.getServiceBySdpProductId(sdpProductId);
-		if (service != null) {
-			Smsuser user = smsUserRepository.getSubscriberByIsdn(service.getId(), "+" + isdn);
-			if (user != null) {
-				user.setActive(0);
-				user.setUnregisterDate(DateHelper.dateToUnixTime(new Date()));
-				smsUserRepository.save(user);
+        } else {
+            log.info("Service not found with SDPPRODUCTID = ", sdpProductId);
+        }
+    }
 
-				log.info("Unregister user {} successfuly.", isdn);
-			} else {
-				log.info("User does not exist on system.");
-			}
+    @Override
+    public void unregister(String isdn, Integer sdpProductId) {
+        log.info("unregister subscriber. isdn = {}, productId = {}", isdn, sdpProductId);
+        ServiceEntity service = serviceRepository.getServiceBySdpProductId(sdpProductId);
+        if (service != null) {
+            Smsuser user = smsUserRepository.getSubscriberByIsdn(service.getId(), "+" + isdn);
+            if (user != null) {
+                user.setActive(0);
+                user.setUnregisterDate(DateHelper.dateToUnixTime(new Date()));
+                smsUserRepository.save(user);
 
-		} else {
-			log.info("Service not found with SDPPRODUCTID = ", sdpProductId);
-		}
-	}
+                log.info("Unregister user {} successfuly.", isdn);
+            } else {
+                log.info("User does not exist on system.");
+            }
 
-	@Override
-	public void subscription(SubscriberRequestDto subRequest) {
-		log.info("subscription subscriber. isdn = {}, productId = {}", subRequest.getIsdn(), subRequest.getProductid());
-		ServiceEntity service = serviceRepository.getServiceBySdpProductId(Integer.valueOf(subRequest.getProductid()));
-		if (service != null) {
-			Smsuser user = smsUserRepository.getSubscriberByIsdn(service.getId(), "+" + subRequest.getIsdn());
-			if (user != null) {
-				user.setLastCharge(DateHelper.dateToUnixTime(new Date()));
-				smsUserRepository.save(user);
+        } else {
+            log.info("Service not found with SDPPRODUCTID = ", sdpProductId);
+        }
+    }
 
-				Charginglog chargingLog = Charginglog.builder().price(Integer.valueOf(subRequest.getAmount()))
-						.date(DateHelper.dateToUnixTime(new Date())).phone("+" + subRequest.getIsdn()).chargeTime(1)
-						.serviceID(service.getId()).reason("SDP VNM Charging")
-						.chargingID(Integer.valueOf(subRequest.getTransid())).build();
-				chargingLogRepository.save(chargingLog);
+    @Override
+    public void subscription(SubscriberRequestDto subRequest) {
+        log.info("subscription subscriber. isdn = {}, productId = {}", subRequest.getIsdn(), subRequest.getProductid());
+        ServiceEntity service = serviceRepository.getServiceBySdpProductId(Integer.valueOf(subRequest.getProductid()));
+        if (service != null) {
+            Smsuser user = smsUserRepository.getSubscriberByIsdn(service.getId(), "+" + subRequest.getIsdn());
+            if (user != null) {
+                user.setLastCharge(DateHelper.dateToUnixTime(new Date()));
+                smsUserRepository.save(user);
 
-				log.info("Subscription user {} successfuly.", subRequest.getIsdn());
-			} else {
-				log.info("User does not exist on system.");
-			}
+                Charginglog chargingLog = Charginglog.builder().price(Integer.valueOf(subRequest.getAmount()))
+                        .date(DateHelper.dateToUnixTime(new Date())).phone("+" + subRequest.getIsdn()).chargeTime(1)
+                        .serviceID(service.getId()).reason("SDP VNM Charging")
+                        .chargingID(Integer.valueOf(subRequest.getTransid())).build();
+                chargingLogRepository.save(chargingLog);
 
-		} else {
-			log.info("Service not found with SDPPRODUCTID = ", subRequest.getProductid());
-		}
-	}
+                log.info("Subscription user {} successfuly.", subRequest.getIsdn());
+            } else {
+                log.info("User does not exist on system.");
+            }
 
-	@Override
-	public List<String> checkSubscription(String isdn) {
-		log.info("Check subscription isdn: {}", isdn);
+        } else {
+            log.info("Service not found with SDPPRODUCTID = ", subRequest.getProductid());
+        }
+    }
 
-		List<String> lstSmsContent = new ArrayList<>();
+    @Override
+    public List<String> checkSubscription(String isdn) {
+        log.info("Check subscription isdn: {}", isdn);
 
-		List<Smsuser> lstSub = smsUserRepository.getAllSubscriptionByIsdn("+" + isdn);
+        List<String> lstSmsContent = new ArrayList<>();
 
-		List<Setting> setting = settingRepository.findAll();
+        List<Smsuser> lstSub = smsUserRepository.getAllSubscriptionByIsdn("+" + isdn);
 
-		String ktdv_success = setting.get(0).getKtdvSuccessMsg();
-		String ktdv_fail = setting.get(0).getKtdvFailMsg();
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss dd-MM-yyyy");
+        List<Setting> setting = settingRepository.findAll();
 
-		if (lstSub.size() > 0) {
-			
-			for (int i = 0; i < lstSub.size(); i++) {
-				ServiceEntity service = serviceRepository.findById(lstSub.get(i).getServiceID()).orElse(null);
-				Charging charging = chargingRepository.findById(service.getChargingID()).orElse(null);
-				ktdv_success = ktdv_success.replace("$name", StringUtils.removeAccent(service.getName())).replace("$date", sdf.format(DateHelper.unixTimeToDate(lstSub.get(i).getRegisterDate())))
-						.replace("$unreg", service.getSyntaxUnregister()).replace("$type", lstSub.get(i).getRegisterType()).replace("$price", String.valueOf(charging.getPrice()));
-				
-				log.info("MT: {}", ktdv_success);
-				
-				lstSmsContent.add(ktdv_success);
-			}
-		} else {
-			lstSmsContent.add(ktdv_fail);
-		}
+        String ktdv_success = setting.get(0).getKtdvSuccessMsg();
+        String ktdv_fail = setting.get(0).getKtdvFailMsg();
 
-		return lstSmsContent;
-	}
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss dd-MM-yyyy");
+
+        if (lstSub.size() > 0) {
+
+            for (int i = 0; i < lstSub.size(); i++) {
+                ServiceEntity service = serviceRepository.findById(lstSub.get(i).getServiceID()).orElse(null);
+                Charging charging = chargingRepository.findById(service.getChargingID()).orElse(null);
+                ktdv_success = ktdv_success.replace("$name", StringUtils.removeAccent(service.getName())).replace("$date", sdf.format(DateHelper.unixTimeToDate(lstSub.get(i).getRegisterDate())))
+                        .replace("$unreg", service.getSyntaxUnregister()).replace("$type", lstSub.get(i).getRegisterType()).replace("$price", String.valueOf(charging.getPrice()));
+
+                log.info("MT: {}", ktdv_success);
+
+                lstSmsContent.add(ktdv_success);
+            }
+        } else {
+            lstSmsContent.add(ktdv_fail);
+        }
+
+        return lstSmsContent;
+    }
 }
